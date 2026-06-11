b5ac263e05d8f7e719fbfde993a030e2dd4fd12c
Josuee16 <josuexpardoch@gmail.com>
2026-06-11 12:12:49 -0500

    Cambios

--- ejemplo07/academia/models.py
+++ ejemplo07/academia/models.py
@@ -1,3 +1,4 @@
+﻿from datetime import date
 from django.db import models
 
 class Estudiante(models.Model):
@@ -6,6 +7,16 @@ class Estudiante(models.Model):
     cedula = models.CharField(max_length=30, unique=True)
     edad = models.IntegerField()
 
+    def año_nacimiento(self):
+        return date.today().year - self.edad
+    
 
-def __str__(self):
-    return f"{self.nombre} - {self.apellido} - {self.cedula} - edad: {self.edad}"
+    def cedula_loja(self):
+        if self.cedula.startswith("11"):
+            return "Loja"
+        else:
+            return "Otra Ciudad"
+      
+
+    def __str__(self):
+        return f"Nombre: {self.nombre} - Apellido: {self.apellido} - CI: {self.cedula_loja()} - Edad: {self.edad} - Año de nacimiento: {self.año_nacimiento()}"
