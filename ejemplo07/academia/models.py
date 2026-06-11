7a25b4f90b172ff147dfa6781b5d4df2cfd8cdb4
Josuee16 <josuexpardoch@gmail.com>
2026-06-11 11:50:27 -0500

    Ejemplo07

---
+++ ejemplo07/academia/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    cedula = models.CharField(max_length=30, unique=True)
+    edad = models.IntegerField()
+
+
+def __str__(self):
+    return f"{self.nombre} - {self.apellido} - {self.cedula} - edad: {self.edad}"
